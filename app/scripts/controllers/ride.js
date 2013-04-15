26c57179423d66323b8dc2b01b012fd63a2ca2a9
Brian Ford <btford@umich.edu>
2013-04-15 13:59:54 -0400

    misc

--- app/scripts/controllers/ride.js
+++ app/scripts/controllers/ride.js
@@ -1,11 +1,21 @@
 'use strict';
 
 angular.module('hitchARideApp')
-  .controller('RideCtrl', function ($scope, socket, $location) {
+  .controller('RideCtrl', function ($scope, socket, $location, geolocation) {
+
+    var now = new Date();
 
     $scope.trip = {
-      from: 'Joplin, MO',
-      to: 'Oklahoma City, OK'
+      from : 'Chicago, IL',
+      to: 'Los Angeles, CA',
+      earliest: {
+        date: (now.getMonth()+1) + '/' + (now.getDate()) + '/' + now.getUTCFullYear(),
+        time: (now.getHours() + 1) + ':00'
+      },
+      latest: {
+        date: (now.getMonth()+1) + '/' + (now.getDate()) + '/' + now.getUTCFullYear(),
+        time: (now.getHours() + 2) + ':00'
+      }
     };
 
     $scope.submit = function () {
@@ -13,4 +23,18 @@ angular.module('hitchARideApp')
         $location.path('/trips');
       });
     };
+
+
+    $scope.geo = function () {
+      geolocation.getCurrentPosition(function (data) {
+        socket.emit('reverse:geocode', {
+          latitude: data.coords.latitude,
+          longitude: data.coords.longitude
+        },
+        function (data) {
+          $scope.trip.from = data;
+        });
+      });
+    };
+
   });
